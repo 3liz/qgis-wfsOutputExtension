7fed631d6bf8647a2ba49c22b37d1e111ddff2a0
David Marteau <dmarteau@3liz.com>
2020-03-20 10:35:46 +0100

    Move sources in its own directory

--- .lizcloud/fabric.mk
+++ .lizcloud/fabric.mk
@@ -6,23 +6,24 @@ ifndef FABRIC
 FABRIC:=$(shell [ -e .fabricrc ] && echo "fab -c .fabricrc" || echo "fab")
 endif
 
-VERSION=$(shell ./metadata_key ../metadata.txt version)
+VERSION=$(shell ./metadata_key ../wfsOutputExtension/metadata.txt version)
 
 main:
 	echo "Makefile for packaging infra components: select a task"
 
 PACKAGE=qgis3_wfsoutput
-FILES = ../*.py ../metadata.txt ../README.md
+FILES = ../wfsOutputExtension/* ../README.md
 PACKAGEDIR=wfsOutputExtension
 
 build2/$(PACKAGEDIR):
 	@rm -rf build2/$(PACKAGEDIR)
 	@mkdir -p build2/$(PACKAGEDIR)
+	@cp -rLp $(FILES) build2/$(PACKAGEDIR)/
 
+dist: build2/$(PACKAGEDIR)
 
 .PHONY: package
-package: build2/$(PACKAGEDIR)
+package: dist
 	@echo "Building package $(PACKAGE)"
-	@cp -rLp $(FILES) build2/$(PACKAGEDIR)/
 	$(FABRIC) package:$(PACKAGE),versiontag=$(VERSION),files=$(PACKAGEDIR),directory=./build2
 
